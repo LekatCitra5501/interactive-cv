831d4495202dce03f5c465ba8d28a255fa4344e6
LekatCitra5501 <lekatcitrafitrianti@students.amikom.ac.id>
2025-07-11 01:23:39 +0700

    fix deploy_

--- api/data.js
+++ api/data.js
@@ -1,16 +1,15 @@
-// backend/data.js
-const educationHistory = [ 
+// api/data.js
+export const educationHistory = [ 
     { id: 1, period: '2020 - 2023', institution: 'SMAN 1 Karangkobar' },
     { id: 2, period: '2023 - Sekarang', institution: 'Universitas Amikom Yogyakarta', major: 'S1 - Informatika' }
 ];
-const skills = [ 
+export const skills = [ 
     { name: 'Vue.js', level: 'Mahir', logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg' },
     { name: 'JavaScript', level: 'Mahir', logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg' },
     { name: 'Tailwind CSS', level: 'Mahir', logo: 'https://www.svgrepo.com/show/374118/tailwind.svg' },
     { name: 'Git & GitHub', level: 'Mahir', logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg' },
 ];
-
-const projects = [ 
+export const projects = [ 
     {
         title: 'Website Toko Online',
         image: '/public/web.png',
@@ -18,7 +17,4 @@ const projects = [
         tech: ['Vue.js', 'Laravel', 'MySQL'],
         link: '#',
     },
-];
-
-export { educationHistory, skills, projects };
-module.exports = { educationHistory, skills, projects }; 
+]; 
